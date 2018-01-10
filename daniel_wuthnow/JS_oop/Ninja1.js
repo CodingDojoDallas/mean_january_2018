bbdf9b0433219c996dc2282168e0fcebdfbbf39c
Daniel <003wuthnowd@gmail.com>
2018-01-10 10:56:31 -0600

    adding ninja1

--- daniel_wuthnow/JS_oop/Ninja1.js
+++ daniel_wuthnow/JS_oop/Ninja1.js
@@ -3,14 +3,30 @@ function ninja(name) {
 
 	const ninja = {};
 
-	this.name = name;
+	ninja.name = name;
+	ninja.health = 100;
+	const speed = 3;
+	const strength = 3;
 
 	ninja.sayName = function() {
-		console.log("Hello my name is " + name)
+		console.log("Hello my name is " + name);
+		return this
+	}
+
+	ninja.showStats = function(){
+		console.log("Name: " + name + ", Health: " + ninja.health + ", Speed: " + speed + ", Strength: " + strength);
+		return this
+		
+	}
+
+	ninja.drinkSake = function(){
+		ninja.health += 10
+		return this
 	}
 
 	return ninja;
 }
 
-const steve = ninja("steve")
-steve.sayName()
+const steve = ninja("steve");
+steve.sayName();
+steve.showStats().drinkSake().showStats()
