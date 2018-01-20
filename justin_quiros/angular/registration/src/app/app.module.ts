2f218c31e1f7c74d4fde5a8b0ec9fe26f240724d
Justin Quiros <jquiros2290@gmail.com>
2018-01-20 13:35:33 -0600

    Angular Registration

--- justin_quiros/angular/registration/src/app/app.module.ts
+++ justin_quiros/angular/registration/src/app/app.module.ts
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
