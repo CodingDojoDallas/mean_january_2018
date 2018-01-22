ec783c45d80f524217088ac5bf4775b8c2ea7719
Rodolfo Valdivieso <valdivieso_rodolfo@hotmail.com>
2018-01-22 10:36:36 -0600

    Assignment Numbers

---
+++ Rodolfo_Valdivieso/angular/services/numbers/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { AlphaComponent } from './alpha/alpha.component';
+import { BetaComponent } from './beta/beta.component';
+import { DataService } from './data.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AlphaComponent,
+    BetaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [DataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
