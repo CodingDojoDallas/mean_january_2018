783ec15c93c5adb85ebdd2b95778fcb1f111ae6e
Arden Zhan <arden@protonmail.ch>
2018-01-23 16:53:13 -0600

    1955 API assignment

---
+++ arden_zhan/MongoDB/Intermediate Mongoose/1955_API/server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json()); 
+
+require('./server/config/mongoose.js');
+var routes_setter = require('./server/config/routes.js');
+routes_setter(app)
+
+app.listen(8000, function() {
+  console.log("listening on port 8000");
+})
