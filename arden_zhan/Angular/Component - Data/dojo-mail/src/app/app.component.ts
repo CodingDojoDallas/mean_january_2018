51f63e6dca153175656bc14c5894ea18918cde90
Arden Zhan <arden@protonmail.ch>
2018-01-24 23:01:18 -0600

    Dojo Mail assignment

---
+++ arden_zhan/Angular/Component - Data/dojo-mail/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  emails: any[] = [
+    {email: 'BILL@GATES.COM', importance: true, subject: 'New Windows', content: 'Windows XI will launch in year 2100'},
+    {email: 'ada@lovelace.com', importance: true, subject: 'Programming', content: 'Enchantress of Numbers'},
+    {email: 'john@carmac.com', importance: false, subject: 'Updated Algo', content: 'New algorithm for shadow volumes.'},
+    {email: 'gabe@newel.com', importance: false, subject: 'HL3!', content: 'Just kidding...'}
+  ]
+}
