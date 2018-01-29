1bbde135477475980a840e96b2be641894efc1fc
Arden Zhan <arden@protonmail.ch>
2018-01-29 15:48:52 -0600

    Layout Assignment

---
+++ arden_zhan/Angular/Architecture/layout/src/app/beta/beta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beta',
+  templateUrl: './beta.component.html',
+  styleUrls: ['./beta.component.css']
+})
+export class BetaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
