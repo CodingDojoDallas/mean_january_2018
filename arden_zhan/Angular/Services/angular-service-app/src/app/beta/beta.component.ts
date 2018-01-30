90aee0aa28e97334a33f5c8723473a5e944e846e
Arden Zhan <arden@protonmail.ch>
2018-01-30 10:49:43 -0600

    Local Data Management Service example

---
+++ arden_zhan/Angular/Services/angular-service-app/src/app/beta/beta.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-beta',
+  templateUrl: './beta.component.html',
+  styleUrls: ['./beta.component.css']
+})
+export class BetaComponent implements OnInit {
+  numbers: number[] = [];
+
+  constructor(private _dataService: DataService) { }
+
+  ngOnInit() {
+    this.numbers = this._dataService.retrieveNumbers();
+  }
+
+}
