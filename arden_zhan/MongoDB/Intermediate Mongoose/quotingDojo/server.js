92ea4d90c0d537546de6c34118ff0eb29a9ec0e7
Arden Zhan <arden@protonmail.ch>
2018-02-02 17:15:53 -0600

    testing, can set require() to a variable

--- arden_zhan/MongoDB/Intermediate Mongoose/quotingDojo/server.js
+++ arden_zhan/MongoDB/Intermediate Mongoose/quotingDojo/server.js
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, './client/views'));
 
 app.set('view engine', 'ejs');
 // require the mongoose configuration file which does the rest for us
-require('./server/config/mongoose.js');
+var mongoose = require('./server/config/mongoose.js');
 // store the function in a variable
 var routes_setter = require('./server/config/routes.js');
 // invoke the function stored in routes_setter and pass it the "app" variable
