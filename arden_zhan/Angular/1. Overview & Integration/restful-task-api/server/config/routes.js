2ce201365daa911732b8c83f173b074bff87f2a6
Arden Zhan <arden@protonmail.ch>
2018-02-20 11:52:03 -0600

    Restful Task API

---
+++ arden_zhan/Angular/1. Overview & Integration/restful-task-api/server/config/routes.js
@@ -0,0 +1,12 @@
+const tasks = require('../controllers/tasks.js');
+const path  = require('path');
+
+module.exports = (app) => {
+    app.get     ('/tasks'       ,   tasks.retrieveAll);
+    app.get     ('/tasks/:id'   ,   tasks.retrieveOne);
+    app.post    ('/tasks'       ,   tasks.create);
+    app.put     ('/tasks/:id'   ,   tasks.update);
+    app.delete  ('/tasks/:id'   ,   tasks.delete);
+    
+    app.all("*", (req, res) => { res.sendFile(path.resolve("./client/dist/index.html")) });
+}
