d4cb8783bdd261bb9b6304de556659e7ca6236bc
Arden Zhan <arden@protonmail.ch>
2018-02-21 22:25:12 -0600

    updated server codes to 400 instead of 500 for bad user input

--- arden_zhan/Angular(R)/restful-task-api/server/controllers/tasks.js
+++ arden_zhan/Angular(R)/restful-task-api/server/controllers/tasks.js
@@ -4,7 +4,7 @@ const Task     = require('../models/task');
 module.exports = {
     retrieveAll: (req, res) => {
         Task.find((err, tasks) => {
-            if (err) return res.status(500).json(err);
+            if (err) return res.status(400).json(err);
             if (tasks == null) return res.status(404).json({message: "No tasks found"});
             return res.json(tasks);
         });
@@ -12,7 +12,7 @@ module.exports = {
 
     retrieveOne: (req, res) => {
         Task.findById(req.params.id, (err, task) => {
-            if (err) return res.status(500).json(err);
+            if (err) return res.status(400).json(err);
             if (task == null) return res.status(404).json({message: "Task ID not found"});
             return res.json(task);
         });
@@ -21,14 +21,14 @@ module.exports = {
     create: (req, res) => {
         const task = new Task(req.body);
         task.save((err) => {
-            if (err) return res.status(500).json(err);
+            if (err) return res.status(400).json(err);
             return res.json(task);
         });
     },
 
     update: (req, res) => {
         Task.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, task) => {
-            if (err) return res.status(500).json(err);
+            if (err) return res.status(400).json(err);
             if (task == null) return res.status(404).json({message: "Task ID not found"});
             return res.json(task);
         });
@@ -36,7 +36,7 @@ module.exports = {
 
     delete: (req, res) => {
         Task.findByIdAndRemove(req.params.id, (err, task) => {
-            if (err) return res.status(500).json(err);
+            if (err) return res.status(400).json(err);
             if (task == null) return res.status(404).json({message: "Task ID not found"});
             return res.json(task);
         });
