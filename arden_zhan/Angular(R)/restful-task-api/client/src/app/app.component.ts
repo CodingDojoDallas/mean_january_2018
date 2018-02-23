80d23a5129937679a797137b4197c9b69b44063f
Arden Zhan <arden@protonmail.ch>
2018-02-22 20:17:00 -0600

    Added all api tasks to service

--- arden_zhan/Angular(R)/restful-task-api/client/src/app/app.component.ts
+++ arden_zhan/Angular(R)/restful-task-api/client/src/app/app.component.ts
@@ -1,14 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'RESTful Task API';
-  constructor(private _httpService: HttpService){
+  tasks: Task[];
 
+  constructor(private _httpService: HttpService){
+    this.tasks = [];
   }
+
+  ngOnInit(){
+    this.getTasksFromService();
+    // this.getOneTaskFromService(this.tasks[0]['_id']);
+    
+    this.createTaskByService({
+      title: "Test Task", 
+      description: "Test task for create, update, delete"
+    });
+
+    this.deleteTaskByService("5a8f76b2ed464f1f40c66af5");
+  }
+
+  getTasksFromService(){
+    this._httpService.getTasks().subscribe(
+      tasks => {
+        this.tasks = tasks;
+        console.log("Got our tasks!", this.tasks);
+      },
+      error => {
+        console.log("Error:", error);
+      }
+    );
+  }
+
+  getOneTaskFromService(taskID){
+    this._httpService.getOneTask(taskID).subscribe(
+      task => {
+        console.log("Retrieved Task:", task);
+      },
+      error => {
+        console.log("Error", error);
+      }
+    );
+  }
+
+  createTaskByService(taskBody){
+    this._httpService.createTask(taskBody).subscribe(
+      task => {
+        console.log("Created Task:", task);
+      },
+      error => {
+        console.log("Error:", error);
+      }
+    );
+  }
+
+  updateTaskByService(taskID, taskBody){
+    this._httpService.updateTask(taskID, taskBody).subscribe(
+      task => {
+        console.log("Updated Task:", task);
+      },
+      error => {
+        console.log("Error:", error);
+      }
+    );
+  }
+
+  deleteTaskByService(taskID){
+    this._httpService.deleteTask(taskID).subscribe(
+      task => {
+        console.log("Deleted Task:", task);
+      },
+      error => {
+        console.log("Error:", error);
+      }
+    )
+  }
+
 }
--- arden_zhan/Angular(R)/restful-task-api/client/src/app/http.service.ts
+++ arden_zhan/Angular(R)/restful-task-api/client/src/app/http.service.ts
@@ -1,60 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Task } from './task';
 
 @Injectable()
 export class HttpService {
 
-  constructor(private _http: HttpClient) {
-    this.getTasks();
-    this.createTask({title: "Test Task", description: "Test task for create, update, delete"});
-    this.deleteTask("5a8e437ef4a1782e648e0fd2");
-  }
+  constructor(private _http: HttpClient) { }
 
   getTasks(){
-    this._http.get('/tasks').subscribe(
-      (tasks) => {
-        console.log("Got our tasks!", tasks);
-        let firstTaskID = tasks[0]["_id"];
-        this.getOneTask(firstTaskID);
-      }
-    )
+    return this._http.get<Task[]>('/tasks');
   }
 
   getOneTask(taskID){
-    this._http.get(`/tasks/${taskID}`).subscribe(
-      (task) => { 
-        console.log("First task", task);
-      }
-    )
+    return this._http.get(`/tasks/${taskID}`);
   }
 
   createTask(taskBody){
-    this._http.post('/tasks', taskBody).subscribe(
-      (task) => {
-        console.log("Created task", task);
-        this.updateTask(task["_id"], {completed: true});
-      }
-    )
+    return this._http.post('/tasks', taskBody);
   }
 
   updateTask(taskID, taskBody){
-    this._http.put(`/tasks/${taskID}`, taskBody).subscribe(
-      (task) => {
-        console.log("Updated task", task);
-        this.deleteTask(task["_id"]);
-      }
-    )
+    return this._http.put(`/tasks/${taskID}`, taskBody);
   }
 
   deleteTask(taskID){
-    this._http.delete(`/tasks/${taskID}`).subscribe(
-      (task) => {
-        console.log("Deleted task", task);
-      },
-      (err) => {
-        console.log("Error", err);
-      }
-    )
+    return this._http.delete(`/tasks/${taskID}`);
   }
 
 }
