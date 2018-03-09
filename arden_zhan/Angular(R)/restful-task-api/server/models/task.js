e080dd58d4091777e7a45d1d83977ee114463a99
Arden Zhan <arden@protonmail.ch>
2018-03-08 18:28:11 -0600

    tested out defaults and trim

--- arden_zhan/Angular(R)/restful-task-api/server/models/task.js
+++ arden_zhan/Angular(R)/restful-task-api/server/models/task.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose');
 var TaskSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: [true, 'Title is required']
+        required: [true, 'Title is required'],
+        trim: true
     },
     description: {
         type: String,
-        default: ""
+        default: "",
+        trim: true
     },
     completed: {
         type: Boolean,
