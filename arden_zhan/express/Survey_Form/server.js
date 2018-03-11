56900db3ba10926a89d35b9d86167a56ad8c607b
Arden Zhan <arden@protonmail.ch>
2018-03-11 00:57:49 -0600

    package.json

--- arden_zhan/express/Survey_Form/server.js
+++ arden_zhan/express/Survey_Form/server.js
@@ -3,8 +3,11 @@ var path = require("path");
 var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(session({secret: 'sessionencrypt'}));
+
 app.use(express.static(path.join(__dirname, "./static")));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
@@ -14,12 +17,10 @@ app.get('/', function(req, res){
 })
 
 app.get('/result', function(req, res){
-    console.log(req.session.result);
     res.render('result', {result: req.session.result});
 })
 
 app.post('/result', function(req, res) {
-    console.log("POST DATA", req.body);
     req.session.result = req.body;
     res.redirect('/result');
 })
