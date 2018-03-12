b20f2012bc1a6510bcbfaa6e03f1127c72f85bc6
Arden Zhan <arden@protonmail.ch>
2018-03-12 17:36:42 -0500

    removed team-manager

--- arden_zhan/full-mean/team-manager/server/config/mongoose.js
+++
@@ -1,4 +0,0 @@
-const mongoose  = require('mongoose');
-      db_name   = "team_manager";
-
-mongoose.connect(`mongodb://localhost/${db_name}`);
--- arden_zhan/full-mean/team-manager/server/config/routes.js
+++
@@ -1,9 +0,0 @@
-const players = require('../controllers/players.js');
-      path  = require('path');
-
-module.exports = (app) => {
-    app.get('/players', players.index);
-    app.post('/players', players.create);
-    
-    app.all("*", (req, res) => { res.sendFile(path.resolve("./client/dist/index.html")) });
-}
