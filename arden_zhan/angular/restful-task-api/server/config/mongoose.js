22cbcff69112a29fb0ba84a9a47a3495818e67c8
Arden Zhan <arden@protonmail.ch>
2018-03-19 14:26:35 -0500

    used mlab for mongodb

--- arden_zhan/angular/restful-task-api/server/config/mongoose.js
+++ arden_zhan/angular/restful-task-api/server/config/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose  = require('mongoose');
 const db_name   = 'restful_task_api';
 
-mongoose.connect(`mongodb://localhost/${db_name}`);
+// mongoose.connect(`mongodb://localhost/${db_name}`);
+mongoose.connect('mongodb://arden:password@ds213229.mlab.com:13229/restful-tasks-api')
